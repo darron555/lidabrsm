ae7297c9649e28af460bbfe42a874a716b1a4816
Dima Shchiglo <darron555@lve.ru>
2016-11-01 22:09:50 +0300

    01/11/2016 v.1.0.4

--- js/main2.js
+++ js/main2.js
@@ -9,28 +9,10 @@ $(document).ready(function() {
   	lazyLoad : true,
     navigation : false
   });
-	$("#owl-example2").owlCarousel({
-		items : 5, //10 items above 1000px browser width
-		itemsDesktop : [1600,4], //5 items between 1000px and 901px
-		itemsDesktopSmall : [900,4], // betweem 900px and 601px
-		itemsTablet: [600,2], //2 items between 600 and 0
-		itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option);
-		lazyLoad : true,
-		navigation : false
-	});
-	$("#owl-example3").owlCarousel({
-		items : 5, //10 items above 1000px browser width
-		itemsDesktop : [1600,4], //5 items between 1000px and 901px
-		itemsDesktopSmall : [900,4], // betweem 900px and 601px
-		itemsTablet: [600,2], //2 items between 600 and 0
-		itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option);
-		lazyLoad : true,
-		navigation : false
-	});
 
 
   jQuery('.nav > li > a').click(function(e){
-		e.preventDefault();
+	
 		jQuery.scrollTo(jQuery(this).attr('href'), 400, { offset:-(jQuery('#top').height()), axis:'y' });
 	});
 
